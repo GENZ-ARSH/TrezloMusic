567814b18e6cb1dcabfde6eab5496da7ef0e5add
GENZ-ARSH <arshtyagi007@gmail.com>
2025-07-23 19:55:57 +0530

    first commit

--- config.py
+++ config.py
@@ -31,19 +31,19 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+API_KEY = getenv("API_KEY", '30DxNexGenBotsc3befe') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/venompratap/VenomMusic",
+    "https://github.com/GENZ-ARSH",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VenomPratap")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Venompratapchat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TrezloMM")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TREZLOSUPPORT")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
